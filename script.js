5d9c66c69be66dd75d54d8c68f34ff7459e60ae4
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-02-06 20:51:55 -0500

    Fixed score counter to show tie game, also fix computerchoice showing before asking player for there move

--- script.js
+++ script.js
@@ -2,22 +2,22 @@
 const RPS = ["Rock", "Paper", "Scissors"]
 
 function game(){
+//Set player choice and prints it out
+playerChoice = prompt("Enter your move");
+console.log("Player move = " + playerChoice) ;
+
 //Pulls random number from array length called rps and stores value in computerNumber
 let computerNumber = Math.floor(Math.random() * RPS.length);
 //Converts the random number from computerNumber and converts it to a string option from RPS
 let computerChoice = RPS[computerNumber];
 console.log("Computer move = " + computerChoice);
-    
-//Set player choice and prints it out
-playerChoice = prompt("Enter your move");
-console.log("Player move = " + playerChoice) ;
 
 //This is the main fuction of the game. Checks player choice vs computer choice and determines a output
 if(playerChoice == "Rock" && computerChoice == "Paper" || playerChoice == "Paper" && computerChoice == "Scissors" || playerChoice == "Scissors" && computerChoice == "Rock"){
     console.log("You Lose");
     computerCounter ++;
 }
-else if(playerChoice = "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper"){
+else if(playerChoice == "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper"){
     console.log("You win!!");
     playerCounter ++;
 }else{
@@ -33,9 +33,9 @@ console.log(game());
 console.log(game());
 console.log(game());
 console.log(game());
-if(playerCounter >= computerCounter){
+if(playerCounter > computerCounter){
     console.log("You're the WINNER!!");
-}else if (playerCounter <= computerCounter){
+}else if (playerCounter < computerCounter){
     console.log("You LOSE!!");
 }else if (playerCounter == computerCounter){
     console.log("TIE GAME");
