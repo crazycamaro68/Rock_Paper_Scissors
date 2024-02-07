ba395fe932eea12f281de323692130a95d18f111
crazycamaro68 <153693685+crazycamaro68@users.noreply.github.com>
2024-02-06 20:03:17 -0500

    Fixed computerChoice random fuction and added the main code for the game to work

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 //RPS Stands for RPS
-const RPS = ["rock", "paper", "scissors"]
-let playerChoice = "";
+const RPS = ["Rock", "Paper", "Scissors"]
 
-const computerChoice = Math.floor(Math.random() * RPS.length);
-console.log(RPS[computerChoice])
+//Set player choice and prints it out
+playerChoice = prompt("Enter your move");
+console.log("Player move = " + playerChoice) ;
+//Pulls random number from array length called rps and stores value in computerNumber
+let computerNumber = Math.floor(Math.random() * RPS.length);
+//Converts the random number from computerNumber and converts it to a string option from RPS
+let computerChoice = RPS[computerNumber];
+console.log("Computer move = " + computerChoice);
+
+//This is the main fuction of the game. Checks player choice vs computer choice and determines a output
+if(playerChoice == "Rock" && computerChoice == "Paper" || playerChoice == "Paper" && computerChoice == "Scissors" || playerChoice == "Scissors" && computerChoice == "Rock"){
+    console.log("You Lose");
+}
+else if(playerChoice = "Rock" && computerChoice == "Scissors" || playerChoice == "Paper" && computerChoice == "Rock" || playerChoice == "Scissors" && computerChoice == "Paper"){
+    console.log("You win!!");
+}else{
+    console.log("Tie!! or invalid input");
+}
